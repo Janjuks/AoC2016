8bac686d2cd5b3cbc4d05b5c5f25f31b5999d244
janis.locans <j.locans@gmail.com>
2017-01-26 01:04:59 +0200

    Day 9 - Part 1 in progress

---
+++ Day9/1.elm
@@ -0,0 +1,79 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import String exposing (..)
+import Day9.Input exposing (exampleInput, puzzleInput)
+
+
+main : Html a
+main =
+    exampleInput
+        |> flip decodeFile ""
+        |> text
+
+
+decodeFile : String -> String -> String
+decodeFile file decodedFile =
+    let
+        openingBracketIndex =
+            file
+                |> indexes "("
+                |> List.head
+                |> Maybe.withDefault -1
+
+        closingBracketIndex =
+            file
+                |> indexes ")"
+                |> List.head
+                |> Maybe.withDefault -1
+
+        ( charCount, repeatCount ) =
+            if openingBracketIndex /= -1 || closingBracketIndex /= -1 then
+                file
+                    |> slice (openingBracketIndex + 1) closingBracketIndex
+                    |> getMarker
+            else
+                ( -1, -1 )
+
+        newDecodedFile =
+            if openingBracketIndex /= -1 || closingBracketIndex /= -1 then
+                file
+                    |> slice 0 openingBracketIndex
+                    |> flip append (getRepeatedPhrase file (closingBracketIndex + 1) charCount repeatCount)
+                    |> flip append decodedFile
+            else
+                decodedFile
+    in
+        if openingBracketIndex /= -1 || closingBracketIndex /= -1 then
+            decodeFile (slice (closingBracketIndex + 1 + charCount) (length file) file) newDecodedFile
+        else
+            newDecodedFile ++ file
+
+
+getMarker : String -> ( Int, Int )
+getMarker marker =
+    let
+        charsAndRepeat =
+            marker
+                |> split "x"
+    in
+        ( charsAndRepeat
+            |> List.head
+            |> Maybe.withDefault ""
+            |> toInt
+            |> Result.withDefault -1
+        , charsAndRepeat
+            |> List.drop 1
+            |> List.head
+            |> Maybe.withDefault ""
+            |> toInt
+            |> Result.withDefault -1
+        )
+
+
+getRepeatedPhrase : String -> Int -> Int -> Int -> String
+getRepeatedPhrase file startIndex charCount repeatCount =
+    file
+        |> slice startIndex (startIndex + charCount)
+        |> repeat repeatCount
+        |> Debug.log "repeatetPhrase"
